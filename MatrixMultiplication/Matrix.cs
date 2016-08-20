a90f71ab062b6680cf404acc9f967da72285e967
Tweety79rw <ryan.wilson2@email.wsu.edu>
2016-08-20 01:27:24 -0700

    init commit

---
+++ MatrixMultiplication/Matrix.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MatrixMultiplication
+{
+    public class Matrix
+    {
+        private List<List<int>> matrix;
+
+        public Matrix()
+        {
+            matrix = new List<List<int>>();
+        }
+        public Matrix(List<List<int>> matrix)
+        {
+            this.matrix = matrix;
+        }
+        public static Matrix operator * (Matrix a, Matrix b)
+        {
+            List<List<int>> aMatrix = a.getMatrix();
+            List<List<int>> bMatrix = b.getMatrix();
+            List<List<int>> resultMatrix = new List<List<int>>();
+            for (int row = 0; row < aMatrix.Count; row++)
+            { 
+                List<int> tempMatrix = new List<int>();
+                for (int col = 0; col < bMatrix[0].Count; col++)
+                {
+                    int tempInt = 0;
+                    for (int inner = 0; inner < aMatrix[0].Count; inner++)
+                    {
+                        tempInt += (aMatrix[row][inner] * bMatrix[inner][col]);
+                    }
+                    tempMatrix.Add(tempInt);
+                }
+                    resultMatrix.Add(tempMatrix);
+            }
+            return new Matrix(resultMatrix);
+        }
+        public void scalarMultiply(int scalar){
+            for(int row = 0;row<this.matrix.Count;row++){
+                for(int col = 0;col<this.matrix[0].Count;col++){
+                    this.matrix[row][col] *= scalar;
+                }
+            }
+        }
+        public static Matrix operator +(Matrix a, Matrix b)
+        {
+            List<List<int>> aMatrix = a.getMatrix();
+            List<List<int>> bMatrix = b.getMatrix();
+            List<List<int>> resultMatrix = new List<List<int>>();
+            for (int row = 0; row < aMatrix.Count; row++)
+            {
+                List<int> tempMatrix = new List<int>();
+                for (int col = 0; col < bMatrix[0].Count; col++)
+                {
+
+                    tempMatrix.Add(aMatrix[row][col] + bMatrix[row][col]);
+
+                }
+                resultMatrix.Add(tempMatrix);
+            }
+            return new Matrix(resultMatrix);
+        }
+
+        public List<List<int>> getMatrix() { return matrix; }
+        public void setMatrix(List<List<int>> matrix) { this.matrix = matrix; }
+        public int getRowCount() { 
+            if(this.matrix != null)
+                return this.matrix.Count;
+            return 0;
+        }
+        public int getColCount() { 
+            if(this.getRowCount() > 0 && this.matrix[0] != null)
+                return this.matrix[0].Count;
+            return 0;
+        }
+        public override bool Equals(System.Object obj)
+        {
+            // If parameter is null return false.
+            if (obj == null)
+            {
+                return false;
+            }
+
+            // If parameter cannot be cast to Point return false.
+            Matrix p = obj as Matrix;
+            if ((System.Object)p == null)
+            {
+                return false;
+            }
+            if (this.getRowCount() != p.getRowCount()) return false;
+            if (this.getColCount() != p.getColCount()) return false;
+            if (this.matrix == null && p.getMatrix() == null) return true;
+            if (this.matrix == null || p.getMatrix() == null) return false;
+            for (int i = 0; i < this.matrix.Count; i++)
+            {
+                if (!listsAreEqual(this.matrix[i], p.getMatrix()[i]))
+                    return false;
+            }
+
+            // Return true if the fields match:
+            return true;
+        }
+        private bool listsAreEqual(List<int> a, List<int> b)
+        {
+            if (a == null && b == null) 
+                return true;
+            if (a == null || b == null)
+                return false;
+            if(a.Count == 0)
+                return true;
+            for (int i = 0; i < a.Count; i++)
+            {
+                if (a[i] != b[i])
+                    return false;
+            }
+            return true;
+        }
+        public override string ToString()
+        {
+            string result = "[";
+            bool outerFirst = true;
+            for (int col = 0; col < this.matrix[0].Count; col++)
+            {
+                if (!outerFirst) result += ",";
+                outerFirst = false;
+                result += "[";
+                bool innerFirst = true;
+                for (int row = 0; row < this.matrix.Count; row++)
+                {
+                    if (!innerFirst) result += ",";
+                    innerFirst = false;
+                    result += this.matrix[row][col];
+
+                }
+                result += "]";
+            }
+
+            return result+"]";
+        }
+    }
+}
---
+++ MatrixTests/MatrixTest.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Threading.Tasks;
+using MatrixMultiplication;
+
+
+namespace MatrixTests
+{
+    using NUnit.Framework;
+    using System.Diagnostics;
+    [TestFixture]
+    public class MatrixTest
+    {
+        Matrix aMatrix;
+        Matrix bMatrix;
+
+        [SetUp]
+        public void TestSetup()
+        {
+            List<List<int>> temp = new List<List<int>>();
+            List<List<int>> temp2 = new List<List<int>>();
+            temp.Add(new List<int>{1,-2});
+            temp.Add(new List<int> { 3, -4 });
+            aMatrix = new Matrix(temp);
+            temp2.Add(new List<int> { -5, 6});
+            temp2.Add(new List<int> { 7, -8});
+            bMatrix = new Matrix(temp2);
+        }
+        [Test]
+        public void ShouldExist()
+        {
+            Assert.NotNull(aMatrix);
+            Assert.NotNull(bMatrix);
+        }
+        [Test]
+        public void ShouldBeEqual()
+        {
+            Assert.IsTrue(aMatrix.Equals(aMatrix));
+        }
+        [Test]
+        public void ShouldMultiplyTwoMatricies()
+        {
+            List<List<int>> temp = new List<List<int>>();
+            temp.Add(new List<int> { -19, 22 });
+            temp.Add(new List<int> { -43, 50 });
+            Matrix expected = new Matrix(temp);
+            Matrix result = aMatrix * bMatrix;
+            Assert.IsTrue(expected.Equals(result));
+        }
+        [Test]
+        public void ShouldAddTwoMatricies()
+        {
+            List<List<int>> temp = new List<List<int>>();
+            temp.Add(new List<int>{-4,4});
+            temp.Add(new List<int>{10,-12});
+            Matrix expected = new Matrix(temp);
+            Matrix result = aMatrix + bMatrix;
+            Assert.IsTrue(expected.Equals(result));
+        }
+        
+        [Test]
+        public void ShouldMultiplyScalar()
+        {
+            List<List<int>> temp = new List<List<int>>();
+            temp.Add(new List<int> { 2, -4 });
+            temp.Add(new List<int> { 6, -8});
+            Matrix expected = new Matrix(temp);
+            aMatrix.scalarMultiply(2);
+            Assert.IsTrue(expected.Equals(aMatrix));
+        }
+        [Test]
+        public void ShouldGetMatrix()
+        {
+            List<List<int>> expected = new List<List<int>>();
+            expected.Add(new List<int> { 1, -2 });
+            expected.Add(new List<int> { 3, -4 });
+            List<List<int>> accual = aMatrix.getMatrix();
+            Assert.AreEqual(expected, accual);
+        }
+        [Test]
+        public void ShouldSetMatrix()
+        {
+            List<List<int>> temp = new List<List<int>>();
+            temp.Add(new List<int> { 2, -4 });
+            temp.Add(new List<int> { 6, -8 });
+            Matrix expected = new Matrix(temp);
+            aMatrix.setMatrix(temp);
+            Assert.IsTrue(expected.Equals(aMatrix));
+        }
+        [Test]
+        public void ShouldPrintMatrix()
+        {
+            string expected = "[[1,3],[-2,-4]]";
+            Assert.AreEqual(expected,aMatrix.ToString());
+        }
+        [Test]
+        public void testListAreEqual()
+        {
+            List<List<int>> temp = new List<List<int>>();
+            temp.Add(new List<int> { 2, -4 });
+            temp.Add(new List<int> { 6, -8 });
+            List<List<int>> temp2 = new List<List<int>>();
+            temp2.Add(new List<int> { 2, -4,3 });
+            temp2.Add(new List<int> { 6, -8,5 });
+            List<List<int>> temp3 = new List<List<int>>();
+            temp3.Add(new List<int> { 2, -2 });
+            temp3.Add(new List<int> { 6, -8 });
+            List<List<int>> nullLists = new List<List<int>>();
+            nullLists.Add(null);
+            nullLists.Add(null);
+            List<List<int>> zeroLists = new List<List<int>>();
+            zeroLists.Add(new List<int>());
+            zeroLists.Add(new List<int>());
+            Matrix expected = new Matrix();
+            Matrix actual = new Matrix();
+            expected.setMatrix(null);
+            Assert.IsTrue(!expected.Equals(actual));
+            actual.setMatrix(null);
+            Assert.IsTrue(expected.Equals(actual));
+            expected.setMatrix(temp);
+            Assert.IsTrue(!expected.Equals(actual));
+            actual.setMatrix(temp3);
+            Assert.IsTrue(!expected.Equals(actual));
+            actual.setMatrix(temp2);
+            Assert.IsTrue(!expected.Equals(actual));
+            Assert.IsTrue(!expected.Equals(null));
+            Assert.IsTrue(!expected.Equals(5));
+            expected.setMatrix(nullLists);
+            Assert.IsTrue(!expected.Equals(actual));
+            Assert.IsTrue(!actual.Equals(expected));
+            actual.setMatrix(nullLists);
+            Assert.IsTrue(expected.Equals(actual));
+            expected.setMatrix(zeroLists);
+            Assert.IsTrue(!expected.Equals(actual));
+            actual.setMatrix(zeroLists);
+            Assert.IsTrue(expected.Equals(actual));
+            expected.setMatrix(temp3);
+            Assert.IsTrue(!expected.Equals(actual));
+        }
+        [TearDown]
+        public void TestTearDown()
+        {
+            aMatrix = null;
+            bMatrix = null;
+        }
+    }
+}
